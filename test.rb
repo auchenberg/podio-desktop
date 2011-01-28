0726fd2459315fbd8d05d5a9dcea4fc44db8d356
Kenneth Auchenberg <kenneth@auchenberg.dk>
2011-01-28 16:02:39 +0100

    added podio logo to tray

--- test.rb
+++
@@ -1,7 +0,0 @@
-require 'tray_application';
-
-app = TrayApplication.new("Deskshot")
-app.icon_filename = 'icon.png'
-app.item('Take Screenshot')  {Screenshot.capture}
-app.item('Exit')              {java.lang.System::exit(0)}
-app.run
